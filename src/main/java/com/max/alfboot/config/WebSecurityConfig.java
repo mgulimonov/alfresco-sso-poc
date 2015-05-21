744a46d3e1f4865f2a8b56f043483e304e3aec44
Maxim Gulimonov <mgulimonov@evelopers.com>
2015-05-21 15:26:04 +0300

    Add sso integration

--- src/main/java/com/max/alfboot/config/WebSecurityConfig.java
+++ src/main/java/com/max/alfboot/config/WebSecurityConfig.java
@@ -2,16 +2,12 @@ package com.max.alfboot.config;
 
 import com.max.alfboot.alfresco.AlfrescoAuthenticationProvider;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.ObjectPostProcessor;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import javax.sql.DataSource;
 
@@ -25,20 +21,27 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private AlfrescoAuthenticationProvider alfrescoAuthenticationProvider;
 
+    @Value("${alfresco.sso}")
+    private String ssoPage;
+
+    @Value("${alfresco.logout}")
+    private String logoutUrl;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .rememberMe().and()
                 .authorizeRequests()
-                .antMatchers("/", "/home", "/auth", "/autologin").permitAll()
+                .antMatchers("/", "/home", "/auth", "/autologin", "/logout").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                .loginPage("/login")
+                .loginPage(ssoPage)
                 .permitAll()
                 .and()
                 .logout()
+                .logoutSuccessUrl(logoutUrl)
                 .permitAll();
     }
 
--- src/main/java/com/max/alfboot/main/Controller.java
+++ src/main/java/com/max/alfboot/main/Controller.java
@@ -1,21 +1,21 @@
 package com.max.alfboot.main;
 
 import com.max.alfboot.alfresco.AlfrescoService;
-import com.max.alfboot.alfresco.AlfrescoUser;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.audit.AuditEvent;
-import org.springframework.boot.actuate.audit.AuditEventRepository;
 import org.springframework.boot.actuate.audit.listener.AuditApplicationEvent;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Base64;
 
 /**
  * Created by Maxim on 16-May-15.
@@ -49,19 +49,22 @@ public class Controller {
         return alfrescoService.alfresco().get(path, String.class);
     }
 
-    @RequestMapping("/autologin")
-    public ModelAndView autologin() {
-        AlfrescoUser alfrescoUser = alfrescoService.retrieveUser(new UsernamePasswordAuthenticationToken("admin", "ctrl4pr1od6"));
-        String redirect = String.format("/auth?username=%s&ticket=%s", alfrescoUser.getUsername(), alfrescoUser.getToken());
-        return new ModelAndView("redirect:" + redirect);
+    @RequestMapping("/auth")
+    public ModelAndView authenticate(@RequestParam("ticket") String ticket, @RequestParam(value = "redirectTo", defaultValue = "/hello") String redirectTo) {
+        String[] result = new String(Base64.getDecoder().decode(ticket)).split(":");
+
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(result[0], result[1]));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        return new ModelAndView("redirect:" + redirectTo);
     }
 
-    @RequestMapping("/auth")
+    /*@RequestMapping("/auth")
     public ModelAndView authenticate(@RequestParam("username") String username, @RequestParam("ticket") String ticket, @RequestParam(value = "redirectTo", defaultValue = "/hello") String redirectTo) {
         Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, ticket));
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
         return new ModelAndView("redirect:" + redirectTo);
-    }
+    }*/
 
 }
